d234436021213945b622343fe9a751904ab97086
RamanaTotada <totadaramana@gamil.com>
2022-10-27 03:33:27 +0000

    My first React Code

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className="bg-container">
+    <h1 className="heading">congratulations</h1>
+    <div className="card-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="image"
+      />
+      ><h1 className="card-heading">Kiran V</h1>
+      <p>vishunu institute of computer and enducation Bimavaram</p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
